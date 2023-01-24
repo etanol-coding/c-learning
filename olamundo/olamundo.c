8a6de855b23f1867ef340f4f25fa889f8b16b8b2
etanol-coding <daniel.rizato@gmail.com>
2023-01-24 20:35:58 -0300

    olamundo mais dificil do mundo

---
+++ olamundo/olamundo.c
@@ -0,0 +1,9 @@
+#include <stdio.h> //adiciona uma biblioteca que adiciona os comandos basicos
+
+int main(){ //todos os comandos devem ser escritos dentro da função main()
+    printf("Ola, mundo!"); //o comando printf da biblioteca stdio.h printa na tela
+    printf("\n"); //   \n quebra a linha
+    printf("oi"); //depois de todo comando use ;
+    return 0;
+}
+//aperte f6 para rodar o .exe do código no terminal
