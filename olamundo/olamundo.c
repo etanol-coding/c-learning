41217a31e89998c55373282f59ddd7f8383ea64c
etanol-coding <daniel.rizato@gmail.com>
2023-01-26 09:01:09 -0300

    comentarios

--- olamundo/olamundo.c
+++ olamundo/olamundo.c
@@ -1,9 +1,10 @@
+// use // para comentar
 #include <stdio.h> //adiciona uma biblioteca que adiciona os comandos basicos
 
 int main(){ //todos os comandos devem ser escritos dentro da função main()
     printf("Ola, mundo!"); //o comando printf da biblioteca stdio.h printa na tela
     printf("\n"); //   \n quebra a linha
     printf("oi"); //depois de todo comando use ;
-    return 0;
+    return 0; //como é uma função, você tem que retornar alguma coisa
 }
 //aperte f6 para rodar o .exe do código no terminal
