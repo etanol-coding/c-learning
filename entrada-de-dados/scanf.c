57bd65b85b79d17ee5661693a1330bef19764ae7
etanol-coding <daniel.rizato@gmail.com>
2023-01-28 23:26:08 -0300

    scanf

--- entrada-de-dados/scanf.c
+++ entrada-de-dados/scanf.c
@@ -1,1 +1,8 @@
 #include <stdio.h>
+int main(){
+        int idade = 0;
+        printf("Digite sua idade:  ");
+        scanf("%d", &idade);
+        printf("Voce tem %d anos", idade);
+    return 0;
+}
