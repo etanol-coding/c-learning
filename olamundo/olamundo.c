1e3cfebad82cbac852e802ab64aba18d66eb3e03
etanol-coding <daniel.rizato@gmail.com>
2023-01-28 22:16:27 -0300

    tudo? kkkk

--- olamundo/olamundo.c
+++ olamundo/olamundo.c
@@ -3,8 +3,9 @@
 
 int main(){ //todos os comandos devem ser escritos dentro da função main()
     printf("Ola, mundo!"); //o comando printf da biblioteca stdio.h printa na tela
-    printf("\n"); //   \n quebra a linha
+    printf("\nlinha pulada\n"); //   \n quebra a linha
     printf("oi"); //depois de todo comando use ;
     return 0; //como é uma função, você tem que retornar alguma coisa
 }
-//aperte f6 para rodar o .exe do código no terminal
+//aperte f6 para rodar o .exe do código
+//aperte f5 para rodar o debug do código
---
+++ saida-de-dados/saida-de-dados.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    printf("ola \n");
+    printf("Numero inteiro: %d \n", 12);
+    printf("Numero real: %f \n", 12.4842746735);
+    printf("Numero real com duas casa: %.2f \n", 12.4842746735);
+    printf("letra: %c \n", 'm');
+    printf("Texto: %s \n", "olatudobemkkkk");
+}
